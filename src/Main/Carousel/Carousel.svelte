e1104933c1fb85bbd250a50533b77a2fa26680c9
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-03-05 18:58:43 -0600

    pull carousel album into its own component

--- src/Main/Carousel/Carousel.svelte
+++ src/Main/Carousel/Carousel.svelte
@@ -1,9 +1,12 @@
 <script>
   import { tweened } from 'svelte/motion'
+  import { cubicInOut } from 'svelte/easing'
+  import CarouselItem from './CarouselItem/CarouselItem.svelte'
+
   export let albums
   export let selectAlbum
   let carouselWidth, itemsWidth
-  let carouselOffset = tweened(0)
+  let carouselOffset = tweened(0, { easing: cubicInOut })
   $: slideDistance = (Math.floor(carouselWidth / 190) - 1) * 190
   $: overflowWidth = itemsWidth - carouselWidth
 
@@ -22,46 +25,36 @@
       ? carouselOffset.set(overflowWidth)
       : carouselOffset.set(nextOffset)
   }
-
-  
 </script>
 
-	<h4>Current Selections</h4>
+<h4>Current Selections</h4>
+<div 
+  class='album-carousel'
+  bind:clientWidth={carouselWidth}>
+  {#if $carouselOffset}
+    <button 
+      class='previous-button'
+      on:click={slideLeft}
+    >left
+    </button>
+  {/if}
   <div 
-    class='album-carousel'
-    bind:clientWidth={carouselWidth}>
-    {#if $carouselOffset}
-      <button 
-        class='previous-button'
-        on:click={slideLeft}
-      >left
-      </button>
-    {/if}
-    <div 
-      class='carousel-items' 
-      style='--carousel-offset:{$carouselOffset}px'
-      bind:clientWidth={itemsWidth}
-    >
-      {#each albums as album}
-        <div class='carousel-item' on:click={ selectAlbum(album.index) } >
-          {#if !album.loading}
-            <img src={album.thumbnail_url} />
-            <div class='album-info'>
-              <h5>{album.title}</h5>
-              <h6>{album.author_name}</h6>
-            </div>
-          {/if}
-        </div>
-      {/each}
-    </div>
-    {#if $carouselOffset != overflowWidth }
-      <button 
-        class='next-button'
-        on:click={slideRight}
-      >right
-      </button>
-    {/if}
+    class='carousel-items' 
+    style='--carousel-offset:{$carouselOffset}px'
+    bind:clientWidth={itemsWidth}
+  >
+    {#each albums as album}
+      <CarouselItem album={album} selectAlbum={selectAlbum} />
+    {/each}
   </div>
+  {#if $carouselOffset != overflowWidth }
+    <button 
+      class='next-button'
+      on:click={slideRight}
+    >right
+    </button>
+  {/if}
+</div>
 
 <style>
   .album-carousel {
@@ -81,30 +74,6 @@
     right: var(--carousel-offset);
   }
   
-  .carousel-item {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    height: 210px;
-    min-width: 150px;
-    background-color: white;
-    border-radius: 5px;
-    margin: 10px;
-    padding: 10px;
-  }
-
-  .album-info {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    height: 100%;
-    padding: 10px;
-  }
-  img {
-    height: 150px;
-    width: 150px;
-  }
-
   button {
     height: 100%;
     opacity: 0.5;
@@ -113,12 +82,11 @@
     z-index: 1;
   }
 
-    .previous-button{
-      left: 0;
-    }
+  .previous-button{
+    left: 0;
+  }
 
-    .next-button {
-      right: 0;
-    }
-
+  .next-button {
+    right: 0;
+  }
 </style>
---
+++ src/Main/Carousel/CarouselItem/CarouselItem.svelte
@@ -0,0 +1,42 @@
+<script>
+  export let album
+  export let selectAlbum
+</script>
+
+<div class='carousel-item' on:click={ selectAlbum(album.index) } >
+  {#if !album.loading}
+    <img src={album.thumbnail_url} />
+    <div class='album-info'>
+      <h5>{album.title}</h5>
+      <h6>{album.author_name}</h6>
+    </div>
+  {/if}
+</div>
+
+<style>
+  .carousel-item {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    height: 210px;
+    min-width: 150px;
+    background-color: white;
+    border-radius: 5px;
+    margin: 10px;
+    padding: 10px;
+    cursor: pointer;
+  }
+  
+  .album-info {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    height: 100%;
+    padding: 10px;
+  }
+  
+  img {
+    height: 150px;
+    width: 150px;
+  }
+</style>
